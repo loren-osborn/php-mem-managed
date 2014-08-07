0f83a4ca0a61101d42e171cee9fc7a285353f68d
Loren Osborn <linux_dr@yahoo.com>
2014-08-06 23:08:17 -0700

    Delineating which classes can cleanly be proxied

--- src/MemManaged/Internals/ProxyClassFactory.php
+++ src/MemManaged/Internals/ProxyClassFactory.php
@@ -2,19 +2,52 @@
 namespace LinuxDr\MemManaged\Internals;
 
 use ReflectionClass;
+use ReflectionProperty;
 
 class ProxyClassFactory
 {
     public function getProxyClassName($obj)
     {
         $classInfo = new ReflectionClass($obj);
-        $interfaceList = $classInfo->getInterfaceNames();
+        $interfaceList = $this->getClassInterfaceList($classInfo);
         $proxyClass = "LinuxDr\\MemManaged\\Internals\\ObjectProxy";
-        if (count($interfaceList) > 0) {
+        $publicProperties = ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED;
+        $inheritanceDisqualifiers =
+            $classInfo->isFinal() ||
+            (count($classInfo->getProperties($publicProperties)) > 0);
+        if (!$inheritanceDisqualifiers) {
+            $proxySpec = array('parent' => $classInfo->getName());
+            $classNameSuffix = sha1(json_encode($proxySpec));
+            $proxyClass .= "\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix;
+        } elseif (count($interfaceList) > 0) {
             $proxySpec = array('interfaces' =>$interfaceList);
             $classNameSuffix = sha1(json_encode($proxySpec));
             $proxyClass .= "\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix;
         }
         return $proxyClass;
     }
+
+    private function getClassInterfaceList($classInfo)
+    {
+        $interfaceReflectionList = $classInfo->getInterfaces();
+        $interfaceList = $classInfo->getInterfaceNames();
+        $interfaceList = array();
+        foreach ($interfaceReflectionList as $item) {
+            $parent = false;
+            foreach ($interfaceReflectionList as $possibleChild) {
+                if ($possibleChild->isSubclassOf($item->getName())) {
+                    $parent = true;
+                }
+            }
+            if (!$parent) {
+                $interfaceList[] = $item->getName();
+            }
+        }
+        sort($interfaceList);
+        return $interfaceList;
+    }
 }
+/*
+$reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED)
+
+*/
--- tests/Internals/ProxyClassFactoryTest.php
+++ tests/Internals/ProxyClassFactoryTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace LinuxDr\MemManaged\Tests\Internals;
 
+use DateTime;
+use ReflectionClass;
 use PHPUnit_Framework_TestCase;
 use LinuxDr\MemManaged\Internals\ProxyClassFactory;
 
@@ -13,10 +15,48 @@ interface ProxyClassFactoryTest_TestInterface
     public function foo();
 }
 
+interface ProxyClassFactoryTest_TestParentInterface
+{
+    public function bar();
+}
+
+interface ProxyClassFactoryTest_TestChildInterface extends ProxyClassFactoryTest_TestParentInterface
+{
+    public function baz();
+}
+
 final class ProxyClassFactoryTest_TestFinalClassHavingInterface
     implements ProxyClassFactoryTest_TestInterface
 {
     public function foo()
+    {
+    }
+}
+
+final class ProxyClassFactoryTest_TestFinalClassHavingAllInterface
+    implements ProxyClassFactoryTest_TestInterface, ProxyClassFactoryTest_TestParentInterface, ProxyClassFactoryTest_TestChildInterface
+{
+    public function foo()
+    {
+    }
+    public function bar()
+    {
+    }
+    public function baz()
+    {
+    }
+}
+
+final class ProxyClassFactoryTest_TestFinalClassHavingAllInterfacesDifferentOrder
+    implements ProxyClassFactoryTest_TestChildInterface, ProxyClassFactoryTest_TestInterface
+{
+    public function foo()
+    {
+    }
+    public function bar()
+    {
+    }
+    public function baz()
     {
     }
 }
@@ -52,5 +92,45 @@ class ProxyClassFactoryTest extends PHPUnit_Framework_TestCase
             $interfaceClassProxyClass,
             "Final classes (or classes wth final methods) that implement interfaces should " .
                 "illicit proxy implementing those interfaces");
+        $finalClassWithAllInterfaces = new ProxyClassFactoryTest_TestFinalClassHavingAllInterface();
+        $finalClassWithAllInterfaces2 = new ProxyClassFactoryTest_TestFinalClassHavingAllInterfacesDifferentOrder();
+        $allInterfacesClassProxyClass = $factory->getProxyClassName($finalClassWithAllInterfaces);
+        $allInterfacesClassProxyClass2 = $factory->getProxyClassName($finalClassWithAllInterfaces2);
+        $proxySpec = array('interfaces' =>
+            array(
+                "LinuxDr\\MemManaged\\Tests\\Internals\\ProxyClassFactoryTest_TestChildInterface",
+                "LinuxDr\\MemManaged\\Tests\\Internals\\ProxyClassFactoryTest_TestInterface"));
+        $classNameSuffix = sha1(json_encode($proxySpec));
+        $this->assertEquals(
+            "LinuxDr\\MemManaged\\Internals\\ObjectProxy\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix,
+            $allInterfacesClassProxyClass,
+            "Final classes (or classes wth final methods) that implement interfaces should " .
+                "illicit proxy implementing those interfaces in an unambiguous order");
+        $this->assertEquals(
+            "LinuxDr\\MemManaged\\Internals\\ObjectProxy\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix,
+            $allInterfacesClassProxyClass2,
+            "Final classes (or classes wth final methods) that implement interfaces should " .
+                "illicit proxy implementing those interfaces in an unambiguous order");
+        $this->assertEquals(
+            $allInterfacesClassProxyClass,
+            $allInterfacesClassProxyClass2,
+            "Final classes (or classes wth final methods) that implement interfaces should " .
+                "illicit proxy implementing those interfaces in an unambiguous order");
+        $normalClassObj = new DateTime();
+        $normalClassProxyClass = $factory->getProxyClassName($normalClassObj);
+        $proxySpec = array('parent' => 'DateTime');
+        $classNameSuffix = sha1(json_encode($proxySpec));
+        $this->assertEquals(
+            "LinuxDr\\MemManaged\\Internals\\ObjectProxy\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix,
+            $normalClassProxyClass,
+            "Normal classes should just be based on the class name.");
+        $normalClassWithPublicPropertiesObj = new ReflectionClass($normalClassObj);
+        $normalClassWithPublicPropertiesProxyClass = $factory->getProxyClassName($normalClassWithPublicPropertiesObj);
+        $proxySpec = array('interfaces' => array('Reflector'));
+        $classNameSuffix = sha1(json_encode($proxySpec));
+        $this->assertEquals(
+            "LinuxDr\\MemManaged\\Internals\\ObjectProxy\\DynamicallyGenerated\\ObjProxy_" . $classNameSuffix,
+            $normalClassWithPublicPropertiesProxyClass,
+            "Normal classes with public properties can't be inherited from properly.");
     }
 }
